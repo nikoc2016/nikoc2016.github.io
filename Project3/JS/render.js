c713a0fe39c575327de73211d5f3fce626703a92
nikoc2016 <nikoc2016@gmail.com>
2017-05-08 23:47:28 -0700

    Beta 1.0

--- Project3/JS/render.js
+++ Project3/JS/render.js
@@ -1,9 +1,27 @@
 $(document).ready(function(){
-	loadPage("HOME.html");
+	loadPage("page0.html");
+
+	$("#navHome").onclick(function(){
+		loadPage("page0.html");
+	});
+	$("#navUnity").onclick(function(){
+		loadPage("page1.html");
+	});
+	$("#navUnreal").onclick(function(){
+		loadPage("page2.html");
+	});
+	$("#navSnake").onclick(function(){
+		loadPage("page3.html");
+	});
+	$("#navJS").onclick(function(){
+		loadPage("page4.html");
+	});
+	$("#navAPI").onclick(function(){
+		loadPage("page5.html");
+	});
 });
 
 function loadPage(pageName){
-
 	$("#mainDisplayPanel").fadeOut("slow");
 	$("#mainDisplayCtx").load("PAGES/" + pageName);
 	$("#mainDisplayPanel").fadeIn("slow");
